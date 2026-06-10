697e76751aa41ae8602ffbb2eb234a0129d01664
Thomas H Jones II <github@xanthia.com>
2026-06-10 12:15:40 -0400

    Fix syntax-error in pre-staged fapolicyd rule
    Closes #74

--- AWSutils.sh
+++ AWSutils.sh
@@ -73,12 +73,18 @@ function ExemptFapolicyd {
   RULE_DIR="/etc/fapolicyd/rules.d"
   RULE_FILE="${RULE_DIR}/30-aws.rules"
 
+  # In case bootstrapper already has rule-file
+  [[ -e ${RULE_FILE} ]] && rm "${RULE_FILE}"
+
+  # Create RULE_DIR as needed
   chroot "${CHROOTMNT}" install -dDm 0755 -o root -g root "${RULE_DIR}"
+
+  # Create RULE_FILE
   chroot "${CHROOTMNT}" install -bDm 0644 -o root -g root <(
     printf "allow perm=any all : dir=/usr/local/aws-cli/v2/ "
-    printf "type=application/x-executable trust 1\n"
+    printf "type=application/x-executable trust=1\n"
     printf "allow perm=any all : dir=/usr/local/aws-cli/v2/ "
-    printf "type=application/x-sharedlib trust 1\n"
+    printf "type=application/x-sharedlib trust=1\n"
   ) "${RULE_FILE}"
 }
 
